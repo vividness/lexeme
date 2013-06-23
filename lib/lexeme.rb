0531836d248dd5b9f06d73e2d00bae3b5691f7b0
mancmelou <mancmelou@me.com>
2013-06-23 14:13:50 -0700

    Including lexeme object

--- lib/lexeme.rb
+++ lib/lexeme.rb
@@ -1,7 +1,20 @@
 require 'lexeme/rule'
 require 'lexeme/ruleset'
 require 'lexeme/token'
+require 'lexeme/lexeme'
 
 module Lexeme
   VERSION = '0.0.1'
+
+  def self.setup
+    @lexer = Lexeme.new unless @lexer
+    yield @lexer if block_given?
+  end
+
+  def self.analyze(source)
+    tokens = @lexer.analyze(source)
+    tokens.each { |t| yield t } if block_given?
+
+    tokens
+  end
 end
