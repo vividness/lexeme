da41106c5ae801cdbb44cf84c9728e60962478a2
mancmelou <mancmelou@me.com>
2013-07-06 10:06:40 -0700

    Adding a core extension to String - #tokenize

---
+++ lib/lexeme/core_extensions.rb
@@ -0,0 +1,19 @@
+class String 
+  def tokenize
+    content = to_s
+
+    Lexeme.define do
+      token :STOP => /^\.$/
+      token :COMA => /^,$/
+      token :QUES => /^\?$/
+      token :EXCL => /^!$/
+      token :QUOT => /^"$/
+      token :APOS => /^'$/
+      token :WORD => /^[\w\-]+$/
+    end
+    
+    Lexeme.analyze do
+      return from_string content
+    end
+  end
+end
