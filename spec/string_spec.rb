0fbe5daa16745e0b3e747acfa9a871ca754408eb
mancmelou <mancmelou@me.com>
2013-07-06 10:12:37 -0700

    Fixing a test for String#tokenize

---
+++ spec/string_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe String do
+  subject { 'My name is Inigo Montoya!' }
+  
+  describe '#tokenize' do
+    it 'tokenizes a string' do
+      tokens = subject.tokenize
+
+      tokens[0].value.should be_eql 'My'
+      tokens[2].value.should be_eql 'is'
+      tokens[4].value.should be_eql 'Montoya'
+      tokens[-1].value.should be_eql '!'
+    end
+  end
+end
