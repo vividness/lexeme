5252cdddd35580d65b65e7bad874565adf82654a
mancmelou <mancmelou@me.com>
2014-03-25 10:52:13 -0700

    Fix tests; Add line parameter

--- spec/token_spec.rb
+++ spec/token_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Lexeme::Token do
-  subject { Lexeme::Token.new('INT', '1234') }
+  subject { Lexeme::Token.new('INT', '1234', 1) }
   
   describe '#initialize' do
     it 'properly sets up an object' do
@@ -11,13 +11,13 @@ describe Lexeme::Token do
 
   describe '#to_text' do
     it 'returns token as string' do
-      subject.to_s.should be_eql 'INT: 1234'
+      subject.to_s.should be_eql '1 => INT : 1234'
     end
   end
 
   describe '#to_array' do
     it 'returns token as array' do
-      subject.to_array.should == ['INT', '1234']
+      subject.to_array.should == ['INT', '1234', 1]
     end
   end
 end
